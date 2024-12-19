9d26dd6488b6e2615aaebb00929db3152b779704
dostee-hemin <dostee.hemin@gmail.com>
2024-12-19 13:21:01 +0300

    Added Player Dashing
    The player can now double click an arrow key to dash in the direction of the arrow key pressed. This disables the hitbox allowing them to pass through obstacles and attacks.

--- Entities/Player.js
+++ Entities/Player.js
@@ -6,15 +6,22 @@ class Player {
         this.position = new p5.Vector(width/2, height-height/5);
         this.velocity = new p5.Vector();
         this.targetVelocity = new p5.Vector();
-        this.maxSpeed = 4;
+        this.maxSpeed = 4;  // In pixels per frame
         
         this.hitbox  = {'type': 'rect', 'w': 10, 'h': 20};
+
+        this.dashDirection = new p5.Vector();
+        this.dashDuration = 150;        // In milliseconds
+        this.dashStartTime;             // In milliseconds
+        this.previousKey;
+        this.previousKeyTimePressed;    // In milliseconds
     }
 
     display() {
         // Move to the player's position
         push();
         translate(this.position.x, this.position.y);
+        scale(this.isDashing() ? 0.8 : 1);
 
         // Draw the ship as a triangle
         fill(255);
@@ -22,17 +29,27 @@ class Player {
         strokeWeight(2);
         triangle(-20,20,0,-20,20,20);
 
-        // Draw the hitbox of the ship for debug purposes
-        stroke(255,0,0,150);
-        strokeWeight(1);
-        fill(255,0,0,50);
-        rectMode(CENTER);
-        rect(0,0,this.hitbox.w, this.hitbox.h);
+        // Draw the hitbox of the ship for debug purposes (only if not dashing)
+        if(!this.isDashing()) {
+            stroke(255,0,0,150);
+            strokeWeight(1);
+            fill(255,0,0,50);
+            rectMode(CENTER);
+            rect(0,0,this.hitbox.w, this.hitbox.h);
+        }
         pop();
     }
 
     update() {
-        this.velocity.lerp(this.targetVelocity, 0.15);
+        // If the player has activated a dash move, set the velocity to the dash direction
+        if (this.isDashing() && millis() - this.dashStartTime < this.dashDuration) {
+            this.velocity.set(this.dashDirection.x, this.dashDirection.y);
+        } 
+        // If the player is not dashing, gradually move the velocity to the target velocity
+        else {
+            this.dashDirection.mult(0);
+            this.velocity.lerp(this.targetVelocity, 0.15);
+        }
         this.position.add(this.velocity);
 
         // Remain within canvas borders
@@ -40,26 +57,53 @@ class Player {
         this.position.y = constrain(this.position.y, this.hitbox.h/2, height-this.hitbox.h/2);
     }
 
-    keyPressed() {
-        // Based on which arrow key is pressed, set the desired velocity to the right direction
+    // Returns true if the player has activated a dash
+    isDashing() {
+        return this.dashDirection.x != 0 || this.dashDirection.y != 0;
+    }
+
+    // Function to utilize the arrow keys to activate dashes and set the target velocity
+    setTargetVelocity(direction, amount) {
+        // If the player has pressed the same key as last time and pressed it quickly (less than 300ms), they have activated a dash
+        if (keyCode == this.previousKey && millis() - this.previousKeyTimePressed < 300) {
+            // Set the x or y direction of the dash
+            if (direction == 'x') this.dashDirection.direction = amount;
+            else this.dashDirection.y = amount;
+            this.dashDirection.mult(this.maxSpeed*2);
+
+            this.dashStartTime = millis();
+            return;
+        }
+
+        // If the player has not dashed, set the desired velocity to the right direction
         // The if-statements are used to scale the dimension perpendicular to a given arrow to align with integer coordinates
+        if (direction == 'x') {
+            if(this.targetVelocity.y != 0) this.targetVelocity.y *= 1 / Math.abs(this.targetVelocity.y);
+            this.targetVelocity.x = amount;
+        } else {
+            if(this.targetVelocity.x != 0) this.targetVelocity.x *= 1 / Math.abs(this.targetVelocity.x);
+            this.targetVelocity.y = amount;
+        }
+
+        // Record this key and the time it was pressed to test for dashes in the future
+        this.previousKeyTimePressed = millis();
+        this.previousKey = keyCode;
+    }
+
+    keyPressed() {
         switch (keyCode) {
             case UP_ARROW:
-                if(this.targetVelocity.x != 0) this.targetVelocity.x *= 1 / Math.abs(this.targetVelocity.x);
-                this.targetVelocity.y = -1
+                this.setTargetVelocity('y', -1);
                 break;
             case DOWN_ARROW:
-                if(this.targetVelocity.x != 0) this.targetVelocity.x *= 1 / Math.abs(this.targetVelocity.x);
-                this.targetVelocity.y = 1
+                this.setTargetVelocity('y', 1);
                 break;
                     
             case LEFT_ARROW:
-                if(this.targetVelocity.y != 0) this.targetVelocity.y *= 1 / Math.abs(this.targetVelocity.y);
-                this.targetVelocity.x = -1
+                this.setTargetVelocity('x', -1);
                 break;
             case RIGHT_ARROW:
-                if(this.targetVelocity.y != 0) this.targetVelocity.y *= 1 / Math.abs(this.targetVelocity.y);
-                this.targetVelocity.x = 1
+                this.setTargetVelocity('x', 1);
                 break;
         }
         // Make sure to scale the velocity to match the max speed
